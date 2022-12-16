8e5c40fbca101fcda1c68b76f77e53c5ceb535ba
jgacc7 <92179013+jgacc7@users.noreply.github.com>
2022-12-16 11:09:35 +0100

    add depends on condition

--- main.tf
+++ main.tf
@@ -63,6 +63,9 @@ resource "azurerm_private_dns_zone_virtual_network_link" "network_link" {
   resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = data.azurerm_private_dns_zone.dns01.name
   virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  depends_on = [
+    data.azurerm_private_dns_zone.dns01
+  ]
 }
 
 resource "azurerm_private_dns_a_record" "arecord" {
